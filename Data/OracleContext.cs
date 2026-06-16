850151ef697debf8ad2a1091c91631ce9bab62d7
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-16 11:54:46 -0500

    commit - OracleContext .

--- Data/OracleContext.cs
+++ Data/OracleContext.cs
@@ -1,6 +1,20 @@
-﻿namespace ApiOracle19c.Data
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace ApiOracle19c.Data
 {
     public class OracleContext
     {
+        private readonly IConfiguration _configuration;
+
+        public OracleContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public OracleConnection GetConnection()
+        {
+            return new OracleConnection(
+                _configuration.GetConnectionString("OracleConnection"));
+        }   
     }
 }
