d3cf986f51613f843c9e17c2282a30f0d86fd867
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-16 11:49:58 -0500

    commit - Cliente .

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -2,5 +2,10 @@
 {
     public class Cliente
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Telefono { get; set; } = "";
+        public DateTime FechaRegistro { get; set; }
     }
 }
